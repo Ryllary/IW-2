8d9c9fe50496c51367ae4f51f9577ba05a44a2fa
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-03-22 12:38:30 -0300

    css

---
+++ Atvexer8/js/script3.js
@@ -0,0 +1,11 @@
+function botao1(){
+    msgbotoes.innerHTML = 'Você clicou no botão 1'
+}
+
+function botao2(){
+    msgbotoes.innerHTML = 'Você clicou no botão 2'
+}
+
+function botao3(){
+    msgbotoes.innerHTML = 'Você clicou no botão 3'
+}
