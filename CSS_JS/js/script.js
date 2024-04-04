422ac8ab37c054bd0891a9a5f367f349a60673f1
ryllary <victoriaryllary103@gmail.com>
2024-04-04 09:08:05 -0300

    index

--- CSS_JS/js/script.js
+++ CSS_JS/js/script.js
@@ -1,59 +1,53 @@
+/* Arquivo JS + alterar CSS */
 
 let titulo = document.querySelector('h1')
-titulo.textContent = 'Aula 09 Manipular CSS'
-titulo.innerHTML = 'Aula Manipular CSS'
+
 let imagem = document.querySelector('#foto')
-imagem.setAttribute('src', 'img/pride-fc.jpg')
-imagem.setAttribute('width', '280px')
-
-
-document.querySelector('h1').style.color = "red";
-titulo.style.color = "red";
-titulo.style.backgroundColor = "#000";
-titulo.style.borderBottom = "2px solid red";
-titulo.style.padding = "0.625rem";
-titulo.style.borderRadius = "5px";
-titulo.style.textAlign = "center";
+imagem.setAttribute('src', 'img/imgpinkdl.jpg')
+imagem.setAttribute('width', '280px', )
 
 let box = document.querySelectorAll('.box')
-box[0].setAttribute('class', 'escura')
-box[0].removeAttribute('class')
 
+/////// MODOS DE COR ///////
 let tela = document.querySelector('main')
 
 let btnDark = document.querySelector('#btdark')
 
 let btnLight = document.querySelector('#btlight')
 
-let btnPink = document.querySelector('#btPink')
+let btnPink = document.querySelector('#btpink')
+
+let btnBlue = document.querySelector('#btblue')
+
+let btnYellow = document.querySelector('#btyellow')
+
+let btnRed= document.querySelector('#btred')
 
 btnDark.addEventListener('click', modoDark)
 btnLight.addEventListener('click', modoLight)
-btnLight.addEventListener('click', modoPink)
+btnPink.addEventListener('click', modoPink)
+
 
 function modoDark() {
-    event.preventDefault();
+    event.preventDefault()
     console.log('modo dark')
-    tela.classList.add("dark");
-    tela.classList.remove("light");
+    tela.classList.add("dark")
+    tela.classList.remove("light")
+    tela.classList.remove("pink")
 }
 
 function modoLight() {
-    event.preventDefault();
+    event.preventDefault()
     console.log('modo light')
-    tela.classList.remove("dark");
-    tela.classList.add("light");
-}
-function modoPinK() {
-    event.preventDefault();
-    console.log('modo pink')
-    tela.classList.remove("dark");
-    tela.classList.remove("light");
-    tela.classList.add("pink");
+    tela.classList.remove("dark")
+    tela.classList.add("light")
+    tela.classList.remove("pink")
 }
 
-titulo.addEventListener('click', mudarId)
-function mudarId() {
-    titulo.id = 'titulo'
-    console.log(titulo.id)
+function modoPink() {
+    event.preventDefault()
+    console.log('modo pink')
+    tela.classList.remove("dark")
+    tela.classList.remove("light")
+    tela.classList.add("pink")
 }
